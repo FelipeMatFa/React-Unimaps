cf598c0f9bb409c3d30db7915adea048f4b8da55
FelipeMatFa <Felipe2006mattosdefarias@gmail.com>
2024-08-28 16:26:54 -0300

    Estudos-de-fixacao

--- src/components/EstudoFixacao.jsx
+++ src/components/EstudoFixacao.jsx
@@ -6,7 +6,7 @@ function EstudoFixacao(){
             <h2>ESTUDOS DE FIXAÇÃO</h2>
             <div className='main-primeira-div'>
                 <section className='main-primeira-div_primeira-sessao'>
-                    <p>Estátisticas de acertos</p>
+                    <p id='primeira-sessao_paragrafo'>Estátisticas de acertos</p>
                     <section className='primeira-sessao_estatisticas'>
                         <div className='primeira-sessao_estatisticas_div'>
                             <p id='primeira-sessao_div_paragrafo'>DIA 1</p>
@@ -30,38 +30,39 @@ function EstudoFixacao(){
                         </div>
                     </section>
                 </section>
-                <section>
-                    <p>Fazer treinamento</p>
-                    <div>
-                        <input type="text" placeholder="Sobre qual conteúdo?"/>
-                        <input type="text" placeholder="Quanto tempo?"/>
-                        <input type="submit"/>
-                    </div>
+                <section className='main-primeira-div_segunda-sessao'>
+                    <p id='segunda-sessao_paragrafo'>Fazer treinamento</p>
+                    <form className='formulario-treinamento'>
+                        <input id='formulario-treinamento_conteudo' type="text" placeholder="Sobre qual conteúdo?"/>
+                        <input id='formulario-treinamento_tempo' type="text" placeholder="Quanto tempo?"/>
+                        <input id='formulario-treinamento_submeter' type="submit"/>
+                    </form>
                 </section>
-                <section>
-                    <p>Ultimos estudos</p>
-                    <div>
-                        <section>
-                            <p>Fisica quantica</p>
-                            <p>Acertos: 14</p>
-                            <div>
-                                <p>ED</p>
+                <section className='main-primeira-div_terceira-sessao'>
+                    <p id='terceira-sessao_paragrafo'>Ultimos estudos</p>
+                    <div className='terceira-sessao_primeira-div'>
+                        <section className='terceira-sessao_primeira-div_sessao-ultimos-estudos'>
+                            <div className='primeira-div_sessao-ultimos-estudos_nota'>
+                                <p>Fisica quantica</p>
+                                <p>Acertos: 14</p>
                             </div>
+                            <p id='primeira-div_sessao-ultimos-estudos_mencao'>ED</p>
                         </section>
-                        <section>
-                            <p>Química orgânica</p>
-                            <p>Acertos: 21</p>
-                            <div>
-                                <p>PD</p>
+                        <section className='terceira-sessao_primeira-div_sessao-ultimos-estudos'>
+                            <div className='primeira-div_sessao-ultimos-estudos_nota'>
+                                <p>Fisica quantica</p>
+                                <p>Acertos: 18</p>
                             </div>
+                            <p id='primeira-div_sessao-ultimos-estudos_mencao'>ED</p>
                         </section>
-                        <section>
-                            <p>Técnico para internet </p>
-                            <p>Acertos: 21</p>
-                            <div>
-                                <p>PD</p>
+                        <section className='terceira-sessao_primeira-div_sessao-ultimos-estudos'>
+                            <div className='primeira-div_sessao-ultimos-estudos_nota'>
+                                <p>Fisica quantica</p>
+                                <p>Acertos: 21</p>
                             </div>
+                            <p id='primeira-div_sessao-ultimos-estudos_mencao'>PD</p>
                         </section>
+                        
                     </div>
                 </section>
             </div>
