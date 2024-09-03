2cdc9f98cb7cf75e017a41f3fd18c9340f4a7313
FelipeMatFa <Felipe2006mattosdefarias@gmail.com>
2024-09-03 17:37:50 -0300

    navegacao-tela-home

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,10 +1,30 @@
 import Header from '../../components/Header'
+import imagens from '../../assets/user.png'
+import "../../styles/Home.css"
 
 function HomePage(){
     return(
         <div>
             <Header></Header>
-            <h1>Home page</h1>
+            <h1 id="main_paragrafo">Bem-vindo {sessionStorage.getItem('user')}!</h1>
+            <section className='main_navegacao'>
+                <div className='main_navegacao_atalhos'>
+                    <img src={imagens} alt="" />
+                    <p>SEU PERFIL</p>
+                </div>
+                <div className='main_navegacao_atalhos'>
+                    <img src={imagens} alt="" />
+                    <p>DICAS</p>
+                </div>
+                <div className='main_navegacao_atalhos'>
+                    <img src={imagens} alt="" />
+                    <p>TAREFAS</p>
+                </div>
+                <div className='main_navegacao_atalhos'>
+                    <img src={imagens} alt="" />
+                    <p>INFORMAÇÕES</p>
+                </div>
+            </section>
         </div>
     )
 }
