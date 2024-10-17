278a63fd501440cc19216f0c0e6e50761a0fe23e
FelipeMatFa <Felipe2006mattosdefarias@gmail.com>
2024-10-17 17:53:07 -0300

    Navegação verificada pelo sessionStorage

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import { Home, Login, LangPage, Mapa, Chat, Enem, Cadastro, Tarefas, Perfil } from './pages';
-import Modal from './utils/Modal'
 
 function App() {
+  const autenticado = sessionStorage.getItem('id');
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/enem' element={<Enem/>} />
-        <Route path='/mapa' element={<Mapa/>} />
-        <Route path='/' element={<LangPage/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/home' element={<Home/>} />
-        <Route path='/chat' element={<Chat/>} />
-        <Route path='/cadastro' element={<Cadastro/>}/>
-        <Route path='/home/perfil' element={<Perfil/>}/>
-        <Route path='/tarefas' element={<Tarefas/>}/>
-        
-        <Route path='/modal' element={<Modal/>}/>
+        <Route path='/' element={<LangPage />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/cadastro' element={<Cadastro />} />
+
+        {autenticado ? (
+          <>
+            <Route path='/home' element={<Home />} />
+            <Route path='/enem' element={<Enem />} />
+            <Route path='/mapa' element={<Mapa />} />
+            <Route path='/chat' element={<Chat />} />
+            <Route path='/home/perfil' element={<Perfil />} />
+            <Route path='/tarefas' element={<Tarefas />} />
+          </>
+        ) : (
+          <Route path='*' element={<Navigate to="/" />} />
+        )}
       </Routes>
     </BrowserRouter>
   );
